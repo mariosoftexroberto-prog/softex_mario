507d05e4cfde386c56ed46298a4e4b9bb0a9a1de
Victor Henrique <mariosftexroberto@gmail.com>
2025-09-02 16:19:22 -0300

    aula sobre class, atributo, método

---
+++ aula8/classe.ts
@@ -0,0 +1,95 @@
+// class Carro {
+//     public marca: String
+//     public cor: String
+//     public placa: String
+//     public modelo: String
+//     public ano: Number
+// } 
+
+class Pessoa {
+    public nome: string
+    public idedade: string
+    public cor: string
+    public altura: string
+    public sexo: string
+    public peso: string
+
+    correr() {
+        console.log("A pessoa está correndo")
+    }
+    falar() {
+        console.log("A pessoa está falando")
+    }
+    comer() {
+        console.log("A pessoa está comendo")
+    }
+    trabalhar() {
+        console.log("A pessoa está trabalhando")
+    }
+    viajar() {
+        console.log("A pessoa está viajando")
+    }
+    estudar() {
+        console.log("A pessoa está estudando")
+    }
+}
+
+class Celular {
+    public marcar: string
+    public cor: string
+    public altura: number
+    public memoria: number
+    public peso: number
+    public sistemaOperacional: string
+
+    ligar() {
+        console.log("O celular está ligando")
+    }
+    desligar() {
+        console.log("O celular está desligando")
+    }
+    carregar() {
+        console.log("O celular está carregando")
+    }
+    fotografar() {
+        console.log("O celular está fotografando")
+    }
+    gravarVideo() {
+        console.log("O celular está gravando um vídeo")
+    }
+    acessarInternet() {
+        console.log("O celular está acessando a internet")
+    }
+
+}
+
+
+class Animal {
+    public nome: string
+    public idedade: string
+    public cor: string
+    public altura: string
+    public sexo: string
+    public peso: string
+
+    correr() {
+        console.log("O animal está correndo")
+    }
+    comer() {
+        console.log("O animal está comendo")
+    }
+    dormir() {
+        console.log("O animal está dormindo")
+    }
+    brincar() {
+        console.log("O animal está brincando")
+    }
+    emitirSom() {
+        console.log("O animal está emitindo um som")
+    }
+    andar() {
+        console.log("O animal está andando")
+    }
+
+}
+
